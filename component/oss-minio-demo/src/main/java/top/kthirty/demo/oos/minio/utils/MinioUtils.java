b873367405a31dcb7c0df274325ff3690879742b
G006648 <leilei.kang@eslink.com>
2020-10-15 20:35:35 +0800

    feat(minio):

---
+++ component/oss-minio-demo/src/main/java/top/kthirty/demo/oos/minio/utils/MinioUtils.java
@@ -0,0 +1,24 @@
+package top.kthirty.demo.oos.minio.utils;
+
+import io.minio.MinioClient;
+
+
+public class MinioUtils {
+    private static MinioClient instance;
+    public static MinioClient getInstance(){
+        if(instance != null){
+            return instance;
+        }
+        instance = MinioClient.builder().endpoint("http://192.168.126.128:9000/").credentials("minioadmin","minioadmin").build();
+        return instance;
+    }
+
+    public static void makeBucket(String bucketName){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
